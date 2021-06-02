997f747d5fbdeb9fb01469cb7b91d849d4dfaeec
Ibrahbader <>
2021-06-02 15:04:22 +0000

    adding js prop

---
+++ index.js
@@ -0,0 +1,17 @@
+alert('welcome to the carWOW')
+var car = prompt('what kind of car do you like?')
+
+if(car == 'sedan')
+{ 
+  alert('you are at the right place, check the categories')
+}
+else if ( car == '4*4')
+{
+ alert(" you are at the right place, check the categories")}
+
+ else { alert ('sorry')
+ }
+
+
+ 
+ 
